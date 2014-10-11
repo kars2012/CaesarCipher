80c75846e146418d507082ff395a24a8ba1c300b
Bryan J Kars <kars@bryans-mbp.att.net>
2014-10-11 00:31:43 -0400

    Initial Commit

---
+++ proj05/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  proj05
+//
+//  Created by Bryan J Kars on 10/11/14.
+//  Copyright (c) 2014 Bryan J Kars. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
